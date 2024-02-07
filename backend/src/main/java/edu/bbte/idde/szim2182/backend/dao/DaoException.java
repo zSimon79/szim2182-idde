052d0e86eddef17fc0a47b582cc90c5811b091ef
zSimon79 <zoltansimon2002@gmail.com>
2024-02-07 13:29:18 +0200

    Deployment2

--- backend/src/main/java/edu/bbte/idde/szim2182/backend/dao/DaoException.java
+++
@@ -1,7 +0,0 @@
-package edu.bbte.idde.szim2182.backend.dao;
-
-public class DaoException extends RuntimeException {
-    public DaoException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
